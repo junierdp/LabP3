d8303327b81ecc0dc02ec4ee725c01d07da9e19c
Junier Damian <jdamian@technearshore.com>
2018-02-24 09:53:31 -0400

    last class, compliting data methods to manipulate database

--- INTEC.Repository/ApplicationContext.cs
+++ INTEC.Repository/ApplicationContext.cs
@@ -1,11 +1,22 @@
 ﻿using System;
+using INTEC.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 namespace INTEC.Repository
 {
     public class ApplicationContext : DbContext
     {
-        public ApplicationContext()
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRecovery> UserReveries { get; set; }
+
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
         {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
